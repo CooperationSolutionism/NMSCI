0a308d0b0da1decac0730bbd30ef853618c9a450
hbybyyang <2451759073@qq.com>
2024-06-28 18:49:35 +0800

    整理测试代码

--- model/test/Main.purs
+++ model/test/Main.purs
@@ -7,26 +7,23 @@ import Effect.Class.Console (log)
 import FFI.ECC (genKey)
 import Model.CyberAccount (CyberAccount(..), generateSecureRandom)
 
-testCyberAccountJson :: CyberAccount -> Effect Unit
-testCyberAccountJson a = log $ "cyberAccount的json字符串表示: " <> show a
-
 main :: Effect Unit
 main = do
-  log "测试cyberAccount的show"
+  log "===== 测试cyberAccount的show ====="
   cyberAccount <- pure $ MakeCyberAccount
     { seed: 1
     , privateKey: ""
     , publicKey: ""
     , cyberAddress: ""
     }
-  testCyberAccountJson (cyberAccount)
+  log $ "cyberAccount的json字符串表示: " <> show cyberAccount
 
-  log "测试生成随机数"
+  log "===== 测试生成随机数 ====="
   randomNum <- generateSecureRandom
-  log $ show randomNum
+  log $ "生成的随机数: " <> show randomNum
 
-  log "测试生成ECC密钥"
+  log "===== 测试生成ECC密钥 ====="
   key <- genKey
-  log $ show key
+  log $ "生成的密钥: " <> show key
 
-  log "测试完成"
+  log "===== 测试完成 ====="
